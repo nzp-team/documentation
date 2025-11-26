83f1f91b76d49bb0d3c182df6ea2eb6d2cdf48a9
katniny <okuwu1680@gmail.com>
2025-11-26 04:05:48 -0700

    Don't convert external markdown to .html links

--- links-to-html.lua
+++ links-to-html.lua
@@ -1,4 +1,6 @@
 function Link(el)
-    el.target = string.gsub(el.target, "%.md", ".html")
+    if not el.target:match("^https?://") then
+      el.target = el.target:gsub("%.md$", ".html")
+    end
     return el
   end
